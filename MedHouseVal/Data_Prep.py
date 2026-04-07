68be447c38fb736aff87f36385bedcd90707e5b2
soheil2017 <soheil.hadadi@gmail.com>
2026-04-07 16:49:46 +0200

    deployment path is created

--- MedHouseVal/Data_Prep.py
+++
@@ -1,9 +0,0 @@
-import ssl
-ssl._create_default_https_context = ssl._create_unverified_context
-from sklearn.datasets import fetch_california_housing
-import pandas as pd
-
-def load_data():
-    data = fetch_california_housing(as_frame=True)
-    df = data.frame
-    return df
